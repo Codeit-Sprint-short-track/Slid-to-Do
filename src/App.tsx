bb2c81c5fa132a79ec040bad46050a300bff7f17
ggjiny <jnss599@gmail.com>
2024-07-22 18:31:28 +0900

    test: husky lint test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { LogoIcon } from '@/assets/index';
 
 function App() {
+  console.log(';oo');
   return (
     <div className="font-Pretendard text-base font-normal">
       <LogoIcon className="w-30 h-20 fill-black" />
