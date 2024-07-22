8ae02779a96d3122e910a9fa6332ecbae15373c6
ggjiny <jnss599@gmail.com>
2024-07-22 18:58:45 +0900

    chore: husky commit할 때 검사 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { LogoIcon } from '@/assets/index';
 
 function App() {
-  console.log(';oo');
   return (
     <div className="font-Pretendard text-base font-normal">
       <LogoIcon className="w-30 h-20 fill-black" />
