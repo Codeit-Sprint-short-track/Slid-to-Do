057108718e7a1e8ed8e2cfa6d91931d8ada792b0
ggjiny <jnss599@gmail.com>
2024-07-23 18:36:59 +0900

    style: 케밥 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,9 @@
+export { ReactComponent as LogoIcon } from './logo.svg';
+export { ReactComponent as TextLogoIcon } from './textLogo.svg';
+
+export { ReactComponent as ProfileIcon } from './profile.svg';
+
 export { ReactComponent as FlagIcon } from './flag.svg';
 export { ReactComponent as FoldIcon } from './fold.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
-export { ReactComponent as LogoIcon } from './logo.svg';
-export { ReactComponent as ProfileIcon } from './profile.svg';
-export { ReactComponent as TextLogoIcon } from './textLogo.svg';
+export { ReactComponent as KebabIcon } from './kebab.svg';
