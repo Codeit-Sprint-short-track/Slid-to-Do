903de2ff3077968c0891a24c061aff1e30dc355b
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-23 14:03:15 +0900

    chore: 사이드바 아이콘 추가 및 컴포넌트화

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,9 @@
+export { ReactComponent as FlagIcon } from './flag.svg';
+export { ReactComponent as HomeIcon } from './home.svg';
+export { ReactComponent as ExpendIcon } from './ic_expand.svg';
+export { ReactComponent as TextLogoIcon } from './img_logo.svg';
 export {
   ReactComponent as LogoIcon,
   ReactComponent as LogoIcon2,
 } from './logo.svg';
+export { ReactComponent as ProfileIcon } from './profile.svg';
