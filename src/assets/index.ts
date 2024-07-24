a6cd56e949ec853a57a216ca3dbd34fd58b77566
Mochachou <frommillersplanet@gmail.com>
2024-07-24 17:44:24 +0900

    feat: todolist

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,14 @@
+export { ReactComponent as ActiveBlue } from './checkbox-active-blue.svg';
+export { ReactComponent as Inactive } from './checkbox-inactive.svg';
 export { ReactComponent as DeleteIcon } from './delete.svg';
+export { ReactComponent as FileIcon } from './file.svg';
 export { ReactComponent as FlagIcon } from './flag.svg';
 export { ReactComponent as FoldIcon } from './fold.svg';
+export { ReactComponent as GoalIcon } from './goal.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
+export { ReactComponent as LinkIcon } from './link.svg';
 export { ReactComponent as LogoIcon } from './logo.svg';
+export { ReactComponent as NoteViewIcon } from './note-view.svg';
+export { ReactComponent as NoteWriteIcon } from './note-write.svg';
 export { ReactComponent as ProfileIcon } from './profile.svg';
 export { ReactComponent as TextLogoIcon } from './textLogo.svg';
