80db261b17f93dc9c1d466231e807741baf34259
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-24 15:18:22 +0900

    chore: plus 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,9 @@
+export { ReactComponent as BluePlusIcon } from './blueplus.svg';
 export { ReactComponent as DeleteIcon } from './delete.svg';
 export { ReactComponent as FlagIcon } from './flag.svg';
 export { ReactComponent as FoldIcon } from './fold.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
 export { ReactComponent as LogoIcon } from './logo.svg';
+export { ReactComponent as PlusIcon } from './plus.svg';
 export { ReactComponent as ProfileIcon } from './profile.svg';
 export { ReactComponent as TextLogoIcon } from './textLogo.svg';
