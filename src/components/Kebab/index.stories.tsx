42a79fad2ccb3f8b082ce59d9b4f5742f19469f5
ggjiny <jnss599@gmail.com>
2024-07-25 13:23:11 +0900

    style: css, 스토리북 수정

--- src/components/Kebab/index.stories.tsx
+++ src/components/Kebab/index.stories.tsx
@@ -1,9 +1,30 @@
+/* eslint-disable no-console */
+
 import { Meta, StoryObj } from '@storybook/react';
 import Kebab from '.';
 
 const meta: Meta<typeof Kebab> = {
   title: 'Components/Kebab',
   component: Kebab,
+  parameters: {
+    layout: 'centered',
+    backgrounds: {
+      values: [
+        {
+          name: 'black',
+          value: 'black',
+        },
+      ],
+    },
+  },
+  args: {
+    onEdit: () => {
+      console.log('수정');
+    },
+    onDelete: () => {
+      console.log('삭제');
+    },
+  },
 };
 
 export default meta;
--- src/components/Kebab/index.tsx
+++ src/components/Kebab/index.tsx
@@ -33,14 +33,18 @@ function Kebab({ onEdit, onDelete, isSmall = false }: KebabProps) {
     ? {
         icon: 'w-[14px] h-[14px]',
         circle: 'w-6 h-6',
+        box: 'shadow-sm',
+        button: 'text-sm px-4',
       }
     : {
         icon: 'w-6 h-6',
         circle: 'w-8 h-8',
+        box: 'shadow-sm tablet:shadow-lg',
+        button: 'text-sm tablet:text-lg px-4 tablet:px-5',
       };
 
   return (
-    <div className="relative m-40 flex w-fit flex-col items-end">
+    <div className="relative flex flex-col items-end">
       <div
         className="group relative inline-block cursor-pointer"
         onClick={handleClickKebabIcon}
@@ -52,19 +56,19 @@ function Kebab({ onEdit, onDelete, isSmall = false }: KebabProps) {
       </div>
       {isPopOverOpen && (
         <div
-          className="absolute right-0 top-full z-20 -mr-1 mt-3 flex flex-col rounded-lg bg-white text-sm leading-5 shadow-lg tablet:text-lg tablet:leading-7"
+          className={`absolute right-0 top-full z-20 -mr-1 mt-3 flex flex-col rounded-lg bg-white leading-5 text-slate-700 tablet:leading-7 ${sizeClasses.box}`}
           ref={popOverRef}
         >
           <button
             type="button"
-            className="whitespace-nowrap border-b-2 border-slate-50 px-4 pb-[6px] pt-2"
+            className={`whitespace-nowrap border-b-2 border-slate-50 pb-[6px] pt-2 ${sizeClasses.button}`}
             onClick={handleEditClick}
           >
             수정하기
           </button>
           <button
             type="button"
-            className="whitespace-nowrap px-4 pb-2 pt-[6px]"
+            className={`whitespace-nowrap pb-2 pt-[6px] ${sizeClasses.button}`}
             onClick={handleDeleteClick}
           >
             삭제하기
