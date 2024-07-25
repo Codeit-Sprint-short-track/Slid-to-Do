48a03812eb9fde3a2c7c7b31998f8519a1b21797
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-25 18:13:19 +0900

    feat: 공통 레이아웃 설정
    - 배경색 설정
    - 태블릿일 경우 사이드바만큼 공간 차지

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import { Outlet } from 'react-router-dom';
+import SideBar from '@components/SideBar';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
+  const { pathname } = useLocation();
+
+  const bgColor = () => {
+    if (pathname === '/signin' || pathname === '/signup') {
+      return 'bg-white';
+    }
+    return 'bg-slate-200';
+  };
+
+  useEffect(() => {
+    bgColor();
+  }, [pathname]);
+
   return (
-    <div className="font-Pretendard text-base font-normal">
-      <Outlet />
+    <div
+      className={`h-dvh w-dvw ${bgColor()} font-Pretendard text-base font-normal`}
+    >
+      {pathname !== '/signin' && pathname !== '/signup' && <SideBar />}
+      <div className="ml-0 tablet:ml-[60px] desktop:ml-0">
+        <Outlet />
+      </div>
     </div>
   );
 }
