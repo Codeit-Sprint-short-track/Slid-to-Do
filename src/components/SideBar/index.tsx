ddac064ae12873f7654e3d38e37997d7cf01079c
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-25 18:46:19 +0900

    chore: 플러스 버튼 변경

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -13,7 +13,7 @@ function SideBar() {
     <>
       {width >= 744 ? (
         <>
-          {isOpen && width <= 1902 && (
+          {isOpen && width <= 1920 && (
             <div className="absolute left-0 top-0 h-dvh w-dvw bg-black opacity-50">
               {' '}
             </div>
