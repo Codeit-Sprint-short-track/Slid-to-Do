0575de3c26feb08312f92008c1dd3b7395409e25
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-26 10:42:59 +0900

    refactor: bgColor 변수로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import SideBar from '@components/SideBar';
-import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
 function App() {
   const { pathname } = useLocation();
 
-  const bgColor = () => {
-    if (pathname === '/signin' || pathname === '/signup') {
-      return 'bg-white';
-    }
-    return 'bg-slate-200';
-  };
-
-  useEffect(() => {
-    bgColor();
-  }, [pathname]);
+  const bgColor =
+    pathname === '/signin' || pathname === '/signup'
+      ? 'bg-white'
+      : 'bg-slate-200';
 
   return (
     <div
-      className={`h-dvh w-dvw ${bgColor()} font-Pretendard text-base font-normal`}
+      className={`h-dvh w-dvw ${bgColor} font-Pretendard text-base font-normal`}
     >
       {pathname !== '/signin' && pathname !== '/signup' && <SideBar />}
       <div className="ml-0 tablet:ml-[60px] desktop:ml-0">
