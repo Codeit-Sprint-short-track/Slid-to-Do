5673c58c2a4267e471a9e39513cc7676db0be650
tacowasabii <godseoril812@gmail.com>
2024-07-29 16:38:54 +0900

    style: refactor style

--- src/pages/Dashboard/components/TodosByGoal/index.tsx
+++ src/pages/Dashboard/components/TodosByGoal/index.tsx
@@ -1,10 +1,11 @@
+import { Goal } from '@/types/interface';
 import { FlagIcon } from '@assets';
 import { mockGoalsData } from '../mockData';
 import TodosByGoalBox from './TodosByGoalBox';
 
 function TodosByGoal() {
   return (
-    <div className="mt-6 w-full rounded-xl bg-white px-6 pb-6 pt-4">
+    <div className="mt-4 w-full rounded-xl bg-white px-6 pb-6 pt-4 tablet:mt-6">
       <div className="flex items-center">
         <div className="mr-2 flex h-10 w-10 items-center justify-center rounded-[15px] bg-[#F97316]">
           <FlagIcon fill="white" width={24} height={24} />
@@ -13,14 +14,14 @@ function TodosByGoal() {
           목표별 할 일
         </div>
       </div>
-      {mockGoalsData ? (
-        <div className="mt-4 max-h-[616px] overflow-y-auto">
-          {mockGoalsData.goals.map((goal) => (
+      {mockGoalsData.goals && mockGoalsData.goals.length > 0 ? (
+        <div className="mt-4">
+          {mockGoalsData.goals.map((goal: Goal) => (
             <TodosByGoalBox key={goal.id} title={goal.title} />
           ))}
         </div>
       ) : (
-        <div className="flex h-[616px] items-center justify-center text-sm font-normal text-slate-500">
+        <div className="flex min-h-[200px] items-center justify-center text-sm font-normal text-slate-500 tablet:min-h-[616px]">
           등록한 목표가 없어요
         </div>
       )}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,11 +5,11 @@ import TodosByGoal from './components/TodosByGoal';
 function DashboardPage() {
   return (
     <div className="flex min-h-screen justify-center bg-slate-200 desktop:min-w-[1920px]">
-      <div className="flex w-[375px] flex-col pb-16 pt-6 tablet:w-[637px] desktop:w-[1200px]">
-        <div className="text-lg font-semibold leading-7 text-slate-900">
+      <div className="flex w-[375px] flex-col pb-16 tablet:w-[637px] desktop:w-[1200px]">
+        <h1 className="mt-6 hidden text-lg font-semibold leading-7 text-slate-900 tablet:block">
           대시보드
-        </div>
-        <div className="mt-4 flex w-full flex-col gap-6 tablet:flex-row">
+        </h1>
+        <div className="mt-4 flex w-full flex-col gap-4 tablet:flex-row tablet:gap-6">
           <div className="tablet:min-w-0 tablet:flex-1">
             <RecentTodos />
           </div>
