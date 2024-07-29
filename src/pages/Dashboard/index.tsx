5e2c5df60a4a527efc5149e376546ce984f45b4e
tacowasabii <godseoril812@gmail.com>
2024-07-29 10:20:39 +0900

    style: font color

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,7 +6,9 @@ function DashboardPage() {
   return (
     <div className="flex min-h-screen justify-center bg-slate-200 desktop:min-w-[1920px]">
       <div className="flex w-[375px] flex-col pb-16 pt-6 tablet:w-[637px] desktop:w-[1200px]">
-        <div className="text-lg font-semibold leading-7">대시보드</div>
+        <div className="text-lg font-semibold leading-7 text-slate-900">
+          대시보드
+        </div>
         <div className="mt-4 flex w-full flex-col gap-6 tablet:flex-row">
           <div className="tablet:min-w-0 tablet:flex-1">
             <RecentTodos />
