6e784b8daf52d51a3a4ca6a163923977d469bd76
ggjiny <jnss599@gmail.com>
2024-08-01 13:19:35 +0900

    style: box-shadow xs추가

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -97,7 +97,7 @@ function TodoItem({
               onClick={(e) => e.stopPropagation()}
               aria-label="Open link"
             >
-              <LinkIcon className="cursor-pointer fill-blue-100 stroke-blue-400" />
+              <LinkIcon width={24} height={24} />
             </Link>
           )}
           {todo.noteId !== null ? (
