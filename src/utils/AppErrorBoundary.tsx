53c803477ad37e62b86b06571d8c1242a8c6f869
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-08-20 11:45:47 +0900

    feat: 에러 바운더리 적용

--- src/utils/AppErrorBoundary.tsx
+++ src/utils/AppErrorBoundary.tsx
@@ -1,19 +1,18 @@
-/* eslint-disable */
-
 import { showErrorToast } from '@components/Toast';
 import { isAxiosError } from 'axios';
-import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+import { ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
-function handleFallbackError({ error }: FallbackProps) {
+function handleFallbackError() {
   return (
     <div
       role="alert"
       className="mt-10 flex flex-col items-center justify-center text-2xl"
     >
-      <p>{'오류가 발생했습니다. :('}</p>
+      <p>오류가 발생했습니다. :(</p>
       <button
         type="button"
-        onClick={() => location.reload()}
+        onClick={() => window.location.reload()}
         className="underline"
       >
         돌아가기
@@ -22,27 +21,24 @@ function handleFallbackError({ error }: FallbackProps) {
   );
 }
 
-function AppErrorBoundary({ children }: { children: React.ReactNode }) {
+function AppErrorBoundary({ children }: { children: ReactNode }) {
   const onErrorHandler = (error: Error) => {
-    console.log(error);
     if (isAxiosError(error)) {
       if (
         error.code === 'ERR_CANCELED' ||
         error.response?.data.message === 'Unauthorized'
       ) {
-        // location.href = '/sign-in';
+        window.location.href = '/sign-in';
         showErrorToast('로그인이 필요합니다.');
         return;
       }
       if (error.code === 'ERR_BAD_RESPONSE') {
-        console.log('서버에러가 발생');
         showErrorToast('서버 에러가 발생했습니다.');
         return;
       }
     }
 
     showErrorToast('알 수 없는 에러가 발생했습니다.');
-    return;
   };
   return (
     <ErrorBoundary
